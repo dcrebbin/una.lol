93a4909e0245ab247413072937e4f0013d8d3775
Devon Crebbin <devon@artvuu.group>
2024-06-01 06:09:40 +1000

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "una.lol",
   description: "Open-source models comparison",
+  icons: [
+    {
+      rel: "icon",
+      href: "/favicon.ico",
+      url: "/favicon.ico",
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -16,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
