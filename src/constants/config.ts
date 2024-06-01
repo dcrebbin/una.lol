0c951eaca4b26d4dc0fff2a74b6c8216003dd52d
Devon Crebbin <devon@artvuu.group>
2024-06-02 01:04:58 +1000

    added constants config for llms

---
+++ src/constants/config.ts
@@ -0,0 +1,46 @@
+export const OPEN_AI_MODELS = [
+  {
+    name: "GPT 4o",
+    key: "gpt-4o",
+  },
+  {
+    name: "GPT 4 Turbo",
+    key: "gpt-4-turbo",
+  },
+  {
+    name: "GPT 4",
+    key: "gpt-4",
+  },
+  {
+    name: "GPT 3.5 Turbo",
+    key: "gpt-3.5-turbo",
+  },
+];
+export const ANTHROPIC_MODELS = [
+  {
+    name: "Claude 3 Opus",
+    key: "claude-3-opus-20240229",
+  },
+  {
+    name: "Claude 3 Sonnet",
+    key: "claude-3-sonnet-20240229",
+  },
+  {
+    name: "Claude 3 Haiku",
+    key: "claude-3-haiku-20240307",
+  },
+];
+export const GEMINI_MODELS = [
+  {
+    name: "Gemini 1.5 Pro",
+    key: "gemini-1.5-pro",
+  },
+  {
+    name: "Gemini 1.5 Flash",
+    key: "gemini-1.5-flash",
+  },
+  {
+    name: "Gemini 1 Pro",
+    key: "gemini-1.0-pro",
+  },
+];
